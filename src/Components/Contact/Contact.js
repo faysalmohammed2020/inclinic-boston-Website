1d5a67f87b0c1ad94581df8bafe4639154ce1d0d
Your Name <you@example.com>
2021-10-20 17:31:43 +0600

    Website Completed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,15 +11,15 @@ const Contact = () => {
             <img src ={contactimg} alt="Logo"/>
            </div> 
             <div className="contact-form">
-    
+    <h1 className="text-primary">Contact Us</h1>
     <Form >
       <Form.Group className="mb-3" controlId="formBasicEmail">
        <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
     
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-       <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1"/>
+    
+    <Form.Control as="textarea" rows={3} placeholder="Message"/>
       <Button variant="primary" type="submit" id="Submit-btn">
         Message
       </Button>
